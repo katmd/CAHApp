192c2b77a82d247dd9d35c9a4352da6d555e551e
katmd <mcdaniel.kath@gmail.com>
2018-04-29 12:22:53 -0500

    initial commit

---
+++ app/src/main/java/com/github/katmd/cahapp/ComicActivity.java
@@ -0,0 +1,13 @@
+package com.github.katmd.cahapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comic);
+    }
+}
---
+++ app/src/main/java/com/github/katmd/cahapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.github.katmd.cahapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the View that shows the Official Website ImageButton
+        ImageButton officialWebsite = (ImageButton) findViewById(R.id.go_to_website);
+
+        // Set click listener on the Official Website ImageButton
+        officialWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent officialWebsiteIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.calvinandhobbes.com/") );
+                if (officialWebsiteIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(officialWebsiteIntent);
+                }
+            }
+        });
+    }
+}
