d7622f3867e7939c59b855f6cf659c92f531c42a
katmd <mcdaniel.kath@gmail.com>
2018-05-30 22:48:04 -0500

    Added Glide image loader and revised Comics layout.

--- app/src/main/java/com/github/katmd/cahapp/ComicsFragment.java
+++ app/src/main/java/com/github/katmd/cahapp/ComicsFragment.java
@@ -4,11 +4,15 @@ package com.github.katmd.cahapp;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -19,22 +23,13 @@ import java.net.URL;
  */
 public class ComicsFragment extends Fragment {
 
+    private static String TAG = "Image Loader Output";
+    
     public ComicsFragment() {
         // Required empty public constructor
     }
 
-    private String websiteImage = "https://assets.amuniversal.com/feab55401c17013627b6005056a9545d";
-
-    // Load an image from a known website URL
-    public static Drawable LoadImageFromWeb(String url) {
-        try {
-            InputStream is = (InputStream) new URL(url).getContent();
-            Drawable d = Drawable.createFromStream(is, "Comic Strip Image");
-            return d;
-        } catch (Exception e) {
-            return null;
-        }
-    }
+    private String websiteImage = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,10 +37,17 @@ public class ComicsFragment extends Fragment {
         // Inflate the layout for this fragment
         View comicView = inflater.inflate(R.layout.fragment_comics, container, false);
 
-        // Find the View that shows the Comic Strip ImageView
+        // Find the View that shows the Comic Strip ImageView and load an image from a known website URL
         ImageView comicStrip = (ImageView) comicView.findViewById(R.id.comic_strip);
-        comicStrip.setImageDrawable(LoadImageFromWeb(websiteImage));
-
+        try {
+            Glide.with(getContext()).load(websiteImage).into(comicStrip);
+            Log.d(TAG, "websiteImage returned: " + websiteImage);
+        }
+        catch (Exception e) {
+            Log.d(TAG, "websiteImage returned: " + e);
+            Toast.makeText(getActivity(), getString(R.string.error_load_image_toast), Toast.LENGTH_LONG).show();
+            return null;
+        }
         return comicView;
     }
 
